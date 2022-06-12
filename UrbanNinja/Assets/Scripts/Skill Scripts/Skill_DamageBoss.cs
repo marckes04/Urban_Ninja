b764ff790fa9951496a731610ba1dbcf7052775e
marckes04 <maresp95@gmail.com>
2022-06-11 22:37:24 -0500

    first commit

---
+++ UrbanNinja/Assets/Scripts/Skill Scripts/Skill_DamageBoss.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_DamageBoss : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ UrbanNinja/Assets/Scripts/Skill Scripts/Skill_RotateObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_RotateObject : MonoBehaviour
+{
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(x, y,z)*Time.deltaTime);
+
+    }
+}
