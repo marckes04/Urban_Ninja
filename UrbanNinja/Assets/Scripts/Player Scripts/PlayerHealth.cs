fa5d3f6f1b99476633c41ba5b16e680bbfdee0fd
marckes04 <maresp95@gmail.com>
2022-06-19 00:21:43 -0500

    first commit

--- UrbanNinja/Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ UrbanNinja/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -6,14 +6,111 @@ public class PlayerHealth : MonoBehaviour
 {
     public float realHealth;
 
+    private Animator anim;
+    private bool playerDead;
+    private bool playerBeHit;
+
+    //private Slider HealthSlider;
+    //private Text healthText;
+
+    private BossHealth bossHealth;
+    private Transform bossTransform;
+    private bool victory;
+
+    private string ANIMATION_DEAD    = "Dead";
+    private string ANIMATION_ATTACK  = "Attack";
+    private string ANIMATION_VICTORY = "Victory";
+
+    private string BASE_LAYER_DYING = "Base Layer.Dying";
+    private string BASE_LAYER_VICTORY = "Base Layer.Victory";
+
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
+        //healthSlider = GameObject.Find
+        //healthText = GameObject.Find
+
+        // set the healthText and  slider value
+
+        //bossTransform = GameObject.FindGameObjectWithTag("Boss").transform;
+        //bossHealth = bossTransform.gameObject.GetComponent<BossHealth>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (realHealth <= 0)
+        {
+            realHealth = 0;
+            PlayerDying();
+        }
+
+        if (playerDead)
+        {
+            //StopPlayerDeadAnimation();
+        }
+
+        //if(realHealth > 100)
+        //{
+        //    realHealth = 100f;
+        //}
+
+        //if (bossHealth.realHealth <= 0)
+        //{
+        //    //Victory();
+        //}
+
+        if (victory)
+        {
+            StopVictoryAnimation();
+        }
+
+    }
+
+    void PlayerDying()
+    {
+        playerDead = true;
+        anim.SetBool(ANIMATION_DEAD, true);
+        anim.SetBool(ANIMATION_ATTACK, false);
+    }
+
+    void StopPlayerDeadAnimation()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(BASE_LAYER_DYING))
+        {
+            anim.SetBool(ANIMATION_DEAD, false);
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        realHealth -= amount;
+
+        if(realHealth <= 0)
+        {
+            realHealth = 0;
+        }
+
+        if(amount > 0)
+        {
+            //healthText.text = realHealth.ToString();
+            //healthSlider.value = realHealth;
+            playerBeHit = true;
+        }
+    }
+
+    void Victory()
+    {
+        anim.SetBool(ANIMATION_VICTORY, true);
+        victory = true;
+    }
+
+    void StopVictoryAnimation()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(BASE_LAYER_VICTORY))
+        {
+            anim.SetBool(ANIMATION_VICTORY, false);
+        }
     }
 }
