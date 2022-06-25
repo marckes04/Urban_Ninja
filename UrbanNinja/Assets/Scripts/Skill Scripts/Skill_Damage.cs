2cc8682d7a52f90b42c836db80df07da3ea77b2b
marckes04 <maresp95@gmail.com>
2022-06-25 12:41:57 -0500

    first commit

--- UrbanNinja/Assets/Scripts/Skill Scripts/Skill_Damage.cs
+++ UrbanNinja/Assets/Scripts/Skill Scripts/Skill_Damage.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Skill_Damage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public LayerMask zombieLayer;
+    public float radius;
+    public float damageCount;
+    public GameObject DamageEffect;
 
-    // Update is called once per frame
+    private EnemyHealth attackTarget;
+    private bool collided;
+    
     void Update()
     {
-        
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius,zombieLayer);
+        foreach (Collider c in hits)
+        {
+            if (c.isTrigger)
+            {
+                continue;
+            }
+            attackTarget = c.gameObject.GetComponent<EnemyHealth>();
+            collided = true;
+
+            if (collided)
+            {
+                Instantiate(DamageEffect, transform.position, transform.rotation);
+                attackTarget.EnemyTakeDamage(damageCount);
+            }
+        }
     }
 }
