c3a9cca93469966511f5367451651593143d1d92
marckes04 <maresp95@gmail.com>
2022-06-29 02:37:01 -0500

    first commit

--- UrbanNinja/Assets/Scripts/Player Scripts/Attack Scripts/PlayersAttacks.cs
+++ UrbanNinja/Assets/Scripts/Player Scripts/Attack Scripts/PlayersAttacks.cs
@@ -1,241 +1,280 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayersAttacks : MonoBehaviour
 {
-    public GameObject skillOneEffectPrefab;
-    public GameObject skillOneDamagePrefab;
 
-    public Transform skillOne_Point;
-    public Transform skillOnePoint_1;
-    public Transform skillOnePoint_2;
-    public Transform skillOnePoint_3;
-    public Transform skillOnePoint_4;
-    public Transform skillOnePoint_5;
-    public Transform skillOnePoint_6;
-    public Transform skillOnePoint_7;
-    public Transform skillOnePoint_8;
+	public GameObject skillOne_EffectPrefab;
+	public GameObject skillOne_DamagePrefab;
 
-    public GameObject skillTwo_EffectPrefab;
-    public GameObject skillTwo_DamagePrefab;
+	public Transform skillOne_Point;
 
-    public Transform skillTwo_Point;
+	public Transform skillOnePoint_1;
+	public Transform skillOnePoint_2;
+	public Transform skillOnePoint_3;
+	public Transform skillOnePoint_4;
+	public Transform skillOnePoint_5;
+	public Transform skillOnePoint_6;
+	public Transform skillOnePoint_7;
+	public Transform skillOnePoint_8;
 
-    public Transform skillTwoPoint_1;
-    public Transform skillTwoPoint_2;
-    public Transform skillTwoPoint_3;
-    public Transform skillTwoPoint_4;
-    public Transform skillTwoPoint_5;
-    public Transform skillTwoPoint_6;
+	public GameObject skillTwo_EffectPrefab;
+	public GameObject skillTwo_DamagePrefab;
 
-    public GameObject skillThree_EffectPrefab;
+	public Transform skillTwo_Point;
 
-    public Transform skillThreePoint_1;
-    public Transform skillThreePoint_2;
-    public Transform skillThreePoint_3;
-    public Transform skillThreePoint_4;
-    public Transform skillThreePoint_5;
+	public Transform skillTwoPoint_1;
+	public Transform skillTwoPoint_2;
+	public Transform skillTwoPoint_3;
+	public Transform skillTwoPoint_4;
+	public Transform skillTwoPoint_5;
+	public Transform skillTwoPoint_6;
 
-    public AudioClip skillOneMusic1;
-    public AudioClip skillOneMusic2;
-    public AudioClip playerSkillOneSound;
-    public AudioClip skillTwoMusic;
-    public AudioClip skillThreeMusic;
+	public GameObject skillThree_EffectPrefab;
 
-    private Animator anim;
-    private AudioSource audioSource;
+	public Transform skillThreePoint_1;
+	public Transform skillThreePoint_2;
+	public Transform skillThreePoint_3;
+	public Transform skillThreePoint_4;
+	public Transform skillThreePoint_5;
 
-    private bool s1_NotUsed;
-    private bool s2_NotUsed;
-    private bool s3_NotUsed;
+	public AudioClip skillOneMusic1;
+	public AudioClip skillOneMusic2;
+	public AudioClip playerSkillOneSound;
+	public AudioClip skillTwoMusic;
+	public AudioClip skillThreeMusic;
 
-    private string ANIMATION_ATTACK = "Attack";
-    private string ANIMATION_SKILL_1 = "Skill1";
-    private string ANIMATION_SKILL_2 = "Skill2";
-    private string ANIMATION_SKILL_3 = "Skill3";
+	private Animator anim;
+	private AudioSource audioSource;
 
+	private bool s1_NotUsed;
+	private bool s2_NotUsed;
+	private bool s3_NotUsed;
 
+	// animations states
+	private string ANIMATION_ATTACK = "Attack";
+	private string ANIMATION_SKILL_1 = "Skill1";
+	private string ANIMATION_SKILL_2 = "Skill2";
+	private string ANIMATION_SKILL_3 = "Skill3";
 
+	private Button skillOne_Btn;
+	private Button skillTwo_Btn;
+	private Button skillThree_Btn;
 
-    void Awake()
-    {
-        anim = GetComponent<Animator>();
-        audioSource = GetComponent<AudioSource>();
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+		audioSource = GetComponent<AudioSource>();
 
-        s1_NotUsed = true;
-        s2_NotUsed = true;
-        s3_NotUsed = true;
+		skillOne_Btn = GameObject.Find("Skill One Button").GetComponent<Button>();
+		skillTwo_Btn = GameObject.Find("Skill Two Button ").GetComponent<Button>();
+		skillThree_Btn = GameObject.Find("Skill Three Button ").GetComponent<Button>();
 
-    }
+		skillOne_Btn.onClick.AddListener(() => SkillOneButtonPressed());
+		skillTwo_Btn.onClick.AddListener(() => SkillTwoButtonPressed());
+		skillThree_Btn.onClick.AddListener(() => SkillThreeButtonPressed());
 
-   
-    void Update()
-    {
-        HandleButtonPresses();
-    }
+		s1_NotUsed = true;
+		s2_NotUsed = true;
+		s3_NotUsed = true;
 
-    public void AttackButtonPressed()
-    {
-        anim.SetBool(ANIMATION_ATTACK, true);
-    }
-    public void AttackButtonReleased()
-    {
-        anim.SetBool(ANIMATION_ATTACK, false);
-    }
+	}
 
-    void HandleButtonPresses()
-    {
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            anim.SetBool(ANIMATION_ATTACK, true);
-        }
+	void Update()
+	{
+		HandleButtonPresses();
+	}
 
-        if (Input.GetKeyUp(KeyCode.I))
-        {
-            anim.SetBool(ANIMATION_ATTACK, false);
-        }
+	// LISTENRES FOR BUTTONS BEGIN
 
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            if (s1_NotUsed)
-            {
-                s1_NotUsed = false;
-                anim.SetBool(ANIMATION_SKILL_1, true);
-                StartCoroutine(ResetSkills(1));
-            }
-        }
+	public void AttackButtonPressed()
+	{
+		anim.SetBool(ANIMATION_ATTACK, true);
+	}
 
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            if (s2_NotUsed)
-            {
-                s2_NotUsed = false;
-                anim.SetBool(ANIMATION_SKILL_2,true);
-                StartCoroutine(ResetSkills(2));
+	public void AttackButtonReleased()
+	{
+		anim.SetBool(ANIMATION_ATTACK, false);
+	}
 
-            }
-        }
+	public void SkillOneButtonPressed()
+	{
+		anim.SetBool(ANIMATION_SKILL_1, true);
+	}
 
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            if (s3_NotUsed)
-            {
-                s3_NotUsed = false;
-                anim.SetBool(ANIMATION_SKILL_3, true);
-                StartCoroutine(ResetSkills(3));
+	public void SkillTwoButtonPressed()
+	{
+		anim.SetBool(ANIMATION_SKILL_2, true);
+	}
 
-            }
-        }
-    }
+	public void SkillThreeButtonPressed()
+	{
+		anim.SetBool(ANIMATION_SKILL_3, true);
+	}
 
-    // SKILL EFFECTS
+	// LISTENERS FOR BUTTONS END
 
-    // SKILL ONE
-    void SkillOne(bool SkillOne)
-    {
-        if (SkillOne)
-        {
-            Instantiate(skillOneEffectPrefab,skillOne_Point.position, 
-                skillOne_Point.rotation);
-            audioSource.PlayOneShot(skillOneMusic1);
-            StartCoroutine(SkillOneCoroutine());
-        }
-    }
+	void HandleButtonPresses()
+	{
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			anim.SetBool(ANIMATION_ATTACK, true);
+		}
 
-    void SkillOneSound(bool play)
-    {
-        if (play)
-        {
-            audioSource.PlayOneShot(playerSkillOneSound);
-        }
-    }
+		if (Input.GetKeyUp(KeyCode.I))
+		{
+			anim.SetBool(ANIMATION_ATTACK, false);
+		}
 
-    void SkillOneEnd(bool skillOneEnd)
-    {
-        anim.SetBool(ANIMATION_SKILL_1, false);
-    }
+		if (Input.GetKeyDown(KeyCode.J))
+		{
+			if (s1_NotUsed)
+			{
+				s1_NotUsed = false;
+				anim.SetBool(ANIMATION_SKILL_1, true);
+				StartCoroutine(ResetSkills(1));
+			}
+		}
 
-    IEnumerator SkillOneCoroutine()
-    {
-        yield return new WaitForSeconds(1.5f);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_1.position, skillOnePoint_1.rotation);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_2.position, skillOnePoint_2.rotation);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_3.position, skillOnePoint_3.rotation);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_4.position, skillOnePoint_4.rotation);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_5.position, skillOnePoint_5.rotation);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_6.position, skillOnePoint_6.rotation);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_7.position, skillOnePoint_7.rotation);
-        Instantiate(skillOneDamagePrefab, skillOnePoint_8.position, skillOnePoint_8.rotation);
-    }
+		if (Input.GetKeyDown(KeyCode.K))
+		{
+			if (s2_NotUsed)
+			{
+				s2_NotUsed = false;
+				anim.SetBool(ANIMATION_SKILL_2, true);
+				StartCoroutine(ResetSkills(2));
+			}
+		}
 
-    void SkillTwo(bool skillTwo)
-    {
-        if (skillTwo)
-        {
-            Instantiate(skillTwo_EffectPrefab, skillTwoPoint_1.position, skillTwoPoint_1.rotation);
-            audioSource.PlayOneShot(skillTwoMusic);
-            StartCoroutine(SkillTwoCoroutine());
+		if (Input.GetKeyDown(KeyCode.L))
+		{
+			if (s3_NotUsed)
+			{
+				s3_NotUsed = false;
+				anim.SetBool(ANIMATION_SKILL_3, true);
+				StartCoroutine(ResetSkills(3));
+			}
+		}
 
-        }
-    }
+	}
 
-    void SkillTwoEnd(bool skillTwoEnd)
-    {
-        if (skillTwoEnd)
-        {
-            anim.SetBool(ANIMATION_SKILL_2, false);
-           
-        }
-    }
+	// SKILL EFFECTS
 
-    IEnumerator SkillTwoCoroutine()
-    {
-        yield return new WaitForSeconds(1.5f);
-        Instantiate(skillTwo_DamagePrefab, skillTwoPoint_1.position, skillTwoPoint_1.rotation);
-        Instantiate(skillTwo_DamagePrefab, skillTwoPoint_2.position, skillTwoPoint_2.rotation);
-        Instantiate(skillTwo_DamagePrefab, skillTwoPoint_3.position, skillTwoPoint_3.rotation);
-        Instantiate(skillTwo_DamagePrefab, skillTwoPoint_4.position, skillTwoPoint_4.rotation);
-        Instantiate(skillTwo_DamagePrefab, skillTwoPoint_5.position, skillTwoPoint_5.rotation);
-        Instantiate(skillTwo_DamagePrefab, skillTwoPoint_6.position, skillTwoPoint_6.rotation);
-    }
+	// SKILL ONE
+	void SkillOne(bool skillOne)
+	{
+		if (skillOne)
+		{
+			Instantiate(skillOne_EffectPrefab, skillOne_Point.position,
+				skillOne_Point.rotation);
+			audioSource.PlayOneShot(skillOneMusic1);
+			StartCoroutine(SkillOneCoroutine());
+		}
+	}
 
-    // SKILL THREE
+	void SkillOneSound(bool play)
+	{
+		if (play)
+		{
+			audioSource.PlayOneShot(playerSkillOneSound);
+		}
+	}
 
-    void SkillThree(bool skillThree)
-    {
-        if (skillThree)
-        {
-            Instantiate(skillThree_EffectPrefab, skillThreePoint_1.position, skillThreePoint_1.rotation);
-            Instantiate(skillThree_EffectPrefab, skillThreePoint_2.position, skillThreePoint_2.rotation);
-            Instantiate(skillThree_EffectPrefab, skillThreePoint_3.position, skillThreePoint_3.rotation);
-            Instantiate(skillThree_EffectPrefab, skillThreePoint_4.position, skillThreePoint_4.rotation);
-            Instantiate(skillThree_EffectPrefab, skillThreePoint_5.position, skillThreePoint_5.rotation);
-        }
-    }
+	void SkillOneEnd(bool skillOneEnd)
+	{
+		if (skillOneEnd)
+		{
+			anim.SetBool(ANIMATION_SKILL_1, false);
+		}
+	}
 
-    void SkillThreeEnd(bool skillThreeEnd)
-    {
-        if (skillThreeEnd)
-        {
-            anim.SetBool(ANIMATION_SKILL_3, false);
-        }
-    }
+	IEnumerator SkillOneCoroutine()
+	{
+		yield return new WaitForSeconds(1.5f);
+		audioSource.PlayOneShot(skillOneMusic2);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_1.position, skillOnePoint_1.rotation);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_2.position, skillOnePoint_2.rotation);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_3.position, skillOnePoint_3.rotation);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_4.position, skillOnePoint_4.rotation);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_5.position, skillOnePoint_5.rotation);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_6.position, skillOnePoint_6.rotation);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_7.position, skillOnePoint_7.rotation);
+		Instantiate(skillOne_DamagePrefab, skillOnePoint_8.position, skillOnePoint_8.rotation);
+	}
 
-    IEnumerator ResetSkills(int skill)
-    {
-        yield return new WaitForSeconds(3f);
+	// SKILL TWO
 
-        switch (skill)
-        {
-            case 1:
-                s1_NotUsed = true;
-                break;
-            case 2:
-                s2_NotUsed = true;
-                break;
-            case 3:
-                s3_NotUsed = true;
-                break;
-        }
-    }
-}
+	void SkillTwo(bool skillTwo)
+	{
+		if (skillTwo)
+		{
+			Instantiate(skillTwo_EffectPrefab, skillTwo_Point.position, skillTwo_Point.rotation);
+			audioSource.PlayOneShot(skillTwoMusic);
+			StartCoroutine(SkillTwoCoroutine());
+		}
+	}
+
+	void SkillTwoEnd(bool skillTwoEnd)
+	{
+		if (skillTwoEnd)
+		{
+			anim.SetBool(ANIMATION_SKILL_2, false);
+		}
+	}
+
+	IEnumerator SkillTwoCoroutine()
+	{
+		yield return new WaitForSeconds(1.5f);
+		Instantiate(skillTwo_DamagePrefab, skillTwoPoint_1.position, skillTwoPoint_1.rotation);
+		Instantiate(skillTwo_DamagePrefab, skillTwoPoint_2.position, skillTwoPoint_2.rotation);
+		Instantiate(skillTwo_DamagePrefab, skillTwoPoint_3.position, skillTwoPoint_3.rotation);
+		Instantiate(skillTwo_DamagePrefab, skillTwoPoint_4.position, skillTwoPoint_4.rotation);
+		Instantiate(skillTwo_DamagePrefab, skillTwoPoint_5.position, skillTwoPoint_5.rotation);
+		Instantiate(skillTwo_DamagePrefab, skillTwoPoint_6.position, skillTwoPoint_6.rotation);
+	}
+
+	// SKILL THREE
+
+	void SkillThree(bool skillThree)
+	{
+		if (skillThree)
+		{
+			Instantiate(skillThree_EffectPrefab, skillThreePoint_1.position, skillThreePoint_1.rotation);
+			Instantiate(skillThree_EffectPrefab, skillThreePoint_2.position, skillThreePoint_2.rotation);
+			Instantiate(skillThree_EffectPrefab, skillThreePoint_3.position, skillThreePoint_3.rotation);
+			Instantiate(skillThree_EffectPrefab, skillThreePoint_4.position, skillThreePoint_4.rotation);
+			Instantiate(skillThree_EffectPrefab, skillThreePoint_5.position, skillThreePoint_5.rotation);
+		}
+	}
+
+	void SkillThreeEnd(bool skillThreeEnd)
+	{
+		if (skillThreeEnd)
+		{
+			anim.SetBool(ANIMATION_SKILL_3, false);
+		}
+	}
+
+	IEnumerator ResetSkills(int skill)
+	{
+		yield return new WaitForSeconds(3f);
+
+		switch (skill)
+		{
+			case 1:
+				s1_NotUsed = true;
+				break;
+
+			case 2:
+				s2_NotUsed = true;
+				break;
+
+			case 3:
+				s3_NotUsed = true;
+				break;
+		}
+	}
+
+} //class
